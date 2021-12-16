97814f03ae783dbc1ea26d2834f5dab4ad04905d
Bruno Pinho <brunopinho@outlook.com>
2021-12-16 15:20:53 -0300

    feito middleware checksExistsUserAccount

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const usernameAlreadyExists = users.find((user) => user.username === username);
+
+  if (!usernameAlreadyExists) {
+    return response.status(404).json({ error: 'Username does not exist' });
+  }
+
+  request.user = usernameAlreadyExists;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
